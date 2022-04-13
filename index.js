5a9718cf8fb354d29939ab9d295fa3776adcbc48
Jubail0 <98705208+Jubail0@users.noreply.github.com>
2022-04-13 14:33:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+
+function myFunction1(){
+    document.getElementById("main-back").src="/Images/pngwing.com (9).png";
+    document.getElementById("main-back").classList.remove("waterMelon");
+    document.getElementById("main-back").classList.remove("mango");
+    document.getElementById("main-back").classList.remove("pineApple");
+    document.body.style.backgroundColor="#EF4F4F";
+
+ 
+
+}
+function myFunction2(){
+    document.getElementById("main-back").src="/Images/pngwing.com (52).png"; 
+    document.getElementById("main-back").classList.add("waterMelon");
+    document.body.style.backgroundColor="#B5FE83";
+}
+function myFunction3(){
+    document.getElementById("main-back").src="/Images/pngwing.com (51).png";
+    document.getElementById("main-back").classList.add("mango");
+    document.body.style.backgroundColor = "#FEE440";
+}
+function myFunction4(){
+    document.getElementById("main-back").src="/Images/pngwing.com (50).png";
+    document.getElementById("main-back").classList.add("pineApple");
+    document.body.style.backgroundColor="#FFB72B";
+}
